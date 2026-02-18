18eeac5bd44d101fc4ba909113d0dec5f00806b6
Tomas McGuinness <tomas@tomasmcguinness.com>
2026-02-18 06:56:25 +0000

    Tools to bulk add rooms

--- tools/populate.py
+++ tools/populate.py
@@ -1,10 +1,13 @@
 import csv
 import requests
+import json
+
+radiatorIds = {}
 
 with open ('radiators.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
-        r = requests.post('http://192.168.1.105/api/radiators', json={
+        response = requests.post('http://192.168.1.105/api/radiators', json={
             "name": row[0],
             "mqttName": row[1],
             "type": int(row[2]),
@@ -13,4 +16,21 @@ with open ('radiators.csv', 'r') as file:
             "flowSensorEndpointId": row[5],
             "returnSensorNodeId": int(row[6],0),
             "returnSensorEndpointId": row[7]
+        })
+
+        # Store the radiatorID against the mqttName (dictionary) for later use.
+        response_dict = json.loads(response.text)
+
+        radiatorIds[response_dict['mqttName']] = response_dict['radiatorId']
+
+with open ('rooms.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        r = requests.post('http://192.168.1.105/api/rooms', json={
+            "name": row[0],
+            "targetTemperature": int(row[1]) * 100,
+            "temperatureSensorNodeId": int(row[2]),
+            "temperatureSensorEndpointId": int(row[3]),
+            "heatLossPerDegree": int(row[4]),
+            "radiatorIds": [radiatorIds[row[5]]]
         })
